626f7745714409871dd0e248217e5dfba5a463b2
Ngoc Duc <lnd240998@gmail.com>
2021-09-15 22:30:22 +0700

    first commit

---
+++ resources/views/aesthetic/layout.blade.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    @include('aesthetic.head')
+</head>
+<body>
+    @include('aesthetic.header')
+
+    @include('aesthetic.footer')
+    @include('aesthetic.script')
+</body>
+</html>

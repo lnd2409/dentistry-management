b4a239cb209c9f92e4af4c0dc6ca0d07211735f2
Ngoc Duc <lnd240998@gmail.com>
2021-11-02 23:49:39 +0700

    hot fix

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,36 +13,8 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-       $nhanvien = [
-       [
-        'nv_ten'=>'Trần Thanh Phụng',
-        'nv_sdt'=>'099996969',
-        
-        'nv_diachi'=>'118 Bùi Thị Xuân, NK, CT',
-        'nv_gioitinh'=>'nam',
-        'username'=>'admin',
-        'password' => bcrypt('12345'),
-       ],
-       [
-        'nv_ten'=>'Lê Ngọc Đức',
-        'nv_sdt'=>'099996969',
-        
-        'nv_diachi'=>'118 Bùi Thị Xuân, NK, CT',
-        'nv_gioitinh'=>'Nữ',
-        'username'=>'lnduc',
-        'password' => bcrypt('12345'),
-       ],
-       [
-        'nv_ten'=>'Lê Minh Nghĩa',
-        'nv_sdt'=>'099996969',
-        
-        'nv_diachi'=>'118 Bùi Thị Xuân, NK, CT',
-        'nv_gioitinh'=>'Nữ',
-        'username'=>'lnnghia',
-        'password' => bcrypt('12345'),
-       ],
-
-       ];
-       DB::table('nhanvien')->insert($nhanvien);
+       $this->call(ChucVu::class);
+       $this->call(ChuyenMon::class);
+       $this->call(NhanVien::class);
     }
 }

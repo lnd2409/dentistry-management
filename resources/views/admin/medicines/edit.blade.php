474074f831840a49991abe4cd47cb821a747af99
nghiale1 <minhnghia2311@gmail.com>
2021-11-05 21:13:04 +0700

    fix medicine

--- resources/views/admin/medicines/edit.blade.php
+++ resources/views/admin/medicines/edit.blade.php
@@ -1,9 +1,9 @@
 @extends('admin.template.layout')
 @push('css')
-    
+
 @endpush
 @section('breadcrumb')
-    
+
 @endsection
 @section('content')
 <div class="container-fluid">
@@ -14,14 +14,21 @@
                 @csrf
                 <div class="row">
                     <div class="col-md-3">Tên thuốc</div>
-                    <div class="col-md-9"><input type="text" value="{{$thuoc->t_ten}}" name="t_ten" class="form-control" required></div>
+                    <div class="col-md-9"><input type="text" value="{{$thuoc->thuoc_ten}}" name="thuoc_ten"
+                            class="form-control" required></div>
                     <div class="col-md-3">Hóa chất</div>
-                    <div class="col-md-9"><input type="text" value="{{$thuoc->t_hoachat}}" name="t_hoachat" class="form-control" required></div>
+                    <div class="col-md-9"><input type="text" value="{{$thuoc->thuoc_hoachat}}" name="thuoc_hoachat"
+                            class="form-control" required></div>
+                    <div class="col-md-3">Số lượng</div>
+                    <div class="col-md-9"><input type="number" min="0" value="{{$thuoc->thuoc_soluong}}"
+                            name="thuoc_soluong" class="form-control"></div>
                     <div class="col-md-3">Giá</div>
-                    <div class="col-md-9"><input type="number" min="0" value="{{$thuoc->gia->gt_gia}}" name="gt_gia" class="form-control" required></div>
+                    <div class="col-md-9"><input type="number" min="0" value="{{$thuoc->gia->gt_gia}}" name="gt_gia"
+                            class="form-control" required></div>
                     <div class="col-md-12">
                         <button type="submit" class="btn btn-primary">Lưu</button>
-                        <button type="button" class="btn btn-info" onclick="window.history.go(-1); return false;">Hủy</button>
+                        <button type="button" class="btn btn-info"
+                            onclick="window.history.go(-1); return false;">Hủy</button>
                     </div>
                 </div>
             </form>
@@ -31,5 +38,5 @@
 </div>
 @endsection
 @push('script')
-    
+
 @endpush
